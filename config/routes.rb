9be103398009fcd216af8d3575ea092183a01e81
taip315 <beaeaeat@gmail.com>
2021-02-14 09:26:14 +0900

    devise導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :shops, controllers:{
+    sessions:       'shops/sessions',
+    passwords:      'shops/passwords',
+    registrations:  'shops/registrations'
+  }
+  devise_for :users, controllers: {
+    sessions:       'users/sessions',
+    passwords:      'users/passwords',
+    registrations:  'users/registrations'
+  }
   root to: "messages#index"
+  resources :messages
 end
