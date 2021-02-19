66f71bf5b187af088b96ebb00543e5c759fe08d4
taip315 <beaeaeat@gmail.com>
2021-02-19 15:07:28 +0900

    2つのモデルを使用した、devise_parameter_sanitizerの設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,14 @@
 class ApplicationController < ActionController::Base
-  before_action :configre_permitted_parameters_users, if: :users_controller?
-  before_action :configre_permitted_parameters_shops, if: :shops_controller?
+  protected
 
-  def configre_permitted_parameters_users
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:first_name, :last_name, :first_kana, :last_kana, :birthday, :gender_id])
-  end
-  def configre_permitted_parameters_shops
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:shop_name,:adoministrator, :postal, :prefecture_id, :address, :building, :nearest_station, :genre_id])
+  def devise_parameter_sanitizer
+    if resource_class == User
+      Users::ParameterSanitizer.new(User, :user, params)
+    elsif resource_class == Shop
+      Shops::ParameterSanitizer.new(Shop, :shop, params)
+    else
+      super
+    end
   end
 end
+
