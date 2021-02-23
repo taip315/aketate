4981ad55c06ec33f36f963755b30ce737cd4225e
taip315 <beaeaeat@gmail.com>
2021-02-23 21:59:52 +0900

    ログイン状態による画面遷移の設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_any!
+
+  private
+  def authenticate_any!
+    unless shop_signed_in?
+      authenticate_user!
+    end
+  end
+  
+  
+  
   protected
 
   def devise_parameter_sanitizer
