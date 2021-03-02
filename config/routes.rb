ff6fab337af0206c103b5a615520c90eef93008e
taip315 <beaeaeat@gmail.com>
2021-03-02 16:49:35 +0900

    ルーティング変更

--- config/routes.rb
+++ config/routes.rb
@@ -12,32 +12,28 @@ Rails.application.routes.draw do
 
   root to: "rooms#index"
   resources :rooms do
-    collection do
-      get :index2
-    end
     resources :messages
   end
-
-  resource :messages, only:[:new]
-  resources :users
-  resources :shops do
-    collection do
-      get :search
-    end
-  end
-  resources :reservations
-
   namespace :users do
-    resources :shops, only:[:show] do
+    resources :shops do
       resources :follow_requests, only: [:create, :destroy]
     end
   end
 
   namespace :shops do
     resources :shops do
+      collection do
+        get :search
+      end
       resources :memberships
       post '/follow_requests/:id' => 'follow_requests#allow', as: 'allow'
       resources :follow_requests
     end
   end
+  resources :messages, only:[:new]
+  resources :users
+  
+  resources :reservations
+
+  
 end
