e8af96710579858610404fdcc7b45fdd0a828e11
taip315 <beaeaeat@gmail.com>
2021-03-05 22:39:25 +0900

    投稿詳細、編集、削除機能の実装

--- app/controllers/shops/posts_controller.rb
+++ app/controllers/shops/posts_controller.rb
@@ -21,17 +21,30 @@ class Shops::PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
+
   def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to root_path
+    else
+      render 'edit'
+    end
   end
+
   def destroy
+    post = Post.find(params[:id])
+    post.destroy
+    redirect_to root_path
   end
   def show
+    @post = Post.find(params[:id])
   end
 
   private
   def post_params
-  params.permit(:wine_name,:content,:price, :open_date, :wine_genre_id).merge(shop_id: current_shop.id, sold_out: false)
+  params.require(:post).permit(:wine_name,:content,:price, :open_date, :wine_genre_id).merge(shop_id: current_shop.id, sold_out: false)
   end
 
 end
