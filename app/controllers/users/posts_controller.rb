f506633eb5815ed26d119c482d3b187289af970c
taip315 <beaeaeat@gmail.com>
2021-03-22 17:55:03 +0900

    タグ検索機能（複数ワード）の実装

--- app/controllers/users/posts_controller.rb
+++ app/controllers/users/posts_controller.rb
@@ -1,7 +1,7 @@
 class Users::PostsController < ApplicationController
   skip_before_action :authenticate_any!, only: [:top,:search]
   before_action :redirect_to_toppage, only: :top
-  before_action :set_q, only: [:index,:top,:search]
+  before_action :set_q, only: [:search,:top]
   
   def index
     @posts_all = Post.includes(:shop)
@@ -20,7 +20,32 @@ class Users::PostsController < ApplicationController
   end
 
   def search
-    @posts = @q.result.includes(:shop)
+    keywords = params[:q][:tags_name_cont_all].split(/[[:blank:]]+/)
+    
+    @posts = Post.has_tag_name_all(keywords)
+
+
+    # @posts = Post.includes(:shop)
+    # keywords.each do |keyword|
+    #   next if keyword == ""
+    #   @posts = @posts.joins(:tags).where(tags:{name: "#{keyword}"} )
+    #   binding.pry
+      
+    # end
+    
+    #複数検索
+    # @posts = []
+    # keywords.each do |keyword|
+    #   next if keyword == ""
+    #   @posts += Post.ransack(tags_name_cont: "#{keyword}").result 
+    # end
+    # @posts.uniq!
+
+    # grouping_hash = keywords.reduce({}){
+    #   |hash, word| hash.merge(word=>{tags_name_cont: word})
+    # }
+    # @q = Post.ransack({combinator: 'and', groupings: grouping_hash})
+    # @posts = @q.result.includes(:shop)
   end
 
 
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,4 @@
 class Post < ApplicationRecord
-  
-
-  
   belongs_to :shop
   has_many :comments
   has_many :post_tag_relations
@@ -10,4 +7,10 @@ class Post < ApplicationRecord
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :wine_genre
+
+  scope :has_tag_name_all, -> tag_names{
+    tag_names.map{|tag_name|
+      joins(:tags).merge(Tag.where(name: tag_name))
+      }.inject(:&)
+  }
 end
