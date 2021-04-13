1b269df8ae6d31f2209b202aebb2332021f7c0bc
taip315 <beaeaeat@gmail.com>
2021-04-13 19:05:02 +0900

    shopバリデーション、エラー日本語化

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module Ikitsuke
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-    # config.autoload_paths += %W(#{config.root}/lib) 
+    config.autoload_paths += %W(#{config.root}/lib) 
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml').to_s]
     # Settings in config/environments/* take precedence over those specified here.
